38857b09a412da0554a52a5d9fc8b2480e360e71
Devanshu Yadav <34421869+Blackwatch1910@users.noreply.github.com>
2022-01-04 15:49:53 +0530

    Added solution - LeetHub

---
+++ Run Length Encoding - GFG/run-length-encoding.java
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+import java.util.*;
+ 
+class RLEncoding
+{
+   public static void main(String[] args) 
+   {
+ 
+      Scanner sc = new Scanner(System.in);
+    
+	  int T = sc.nextInt();
+	  sc.nextLine();
+	  while(T>0)
+	  {
+		  
+		String str = sc.nextLine();
+		
+		GfG g = new GfG();
+		System.out.println(g.encode(str));
+		
+      
+        T--;
+	  }
+   }
+}
+
+// } Driver Code Ends
+
+
+class GfG
+ {
+	String encode(String str)
+	{
+          //Your code here
+          String s = "";
+           int count = 1;
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                // System.out.print(str.charAt(i));
+                // System.out.print(count);
+                s = s + str.charAt(i) + count;
+                count=1;
+            }
+        }
+        return s;
+	}
+	
+ }
